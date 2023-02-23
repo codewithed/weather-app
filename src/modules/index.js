b9864bcafa88956185a07320264a6addd76e7b02
Edmund Debrah <edmundking05@gmail.com>
2023-02-23 22:29:15 +0000

    create function for getting wanted data from weather data

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,16 +1,21 @@
 import API_KEY from './api';
 
-let weatherData;
+const usefulData = {};
+
+function getUsefulData(weatherData) {
+  usefulData.city = weatherData.name;
+  usefulData.description = weatherData.weather.description;
+  usefulData.temp = weatherData.main.temp;
+  usefulData.humidity = weatherData.main.humidity;
+  usefulData.windspeed = weatherData.wind.speed;
+}
 
 async function getWeatherData(location) {
   const loc = location;
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${loc}&APPID=${API_KEY}`);
-
-  if (response.ok) {
-    weatherData = await response.json();
-  } else {
-    console.log(`HTTP Response Code: ${response.statusText}`);
-  }
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${loc}&APPID=${API_KEY}`)
+    .then((response) => response.json())
+    .then((data) => getUsefulData(data));
 }
 
 getWeatherData('Cairo');
+console.log(usefulData);
